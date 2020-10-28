c092d7f5bfce9bb0808c70b373684413fb6070b9
Alana Gilston <abluescarab@users.noreply.github.com>
2020-10-28 04:22:54 -0700

    Added 'used equipment' section

--- js/events.js
+++ js/events.js
@@ -1,4 +1,4 @@
-document.getElementById("show-evidence").addEventListener("click", function(evt) {
+document.getElementById("evidence-toggle").addEventListener("click", function(evt) {
     var newState = (evt.target.dataset.state === states.yes.data ?
         states.no : states.yes);
 
@@ -9,7 +9,16 @@ document.getElementById("show-evidence").addEventListener("click", function(evt)
 
 document.getElementById("reset").addEventListener("click", function(evt) {
     var buttons = document.getElementsByClassName("tristate");
+    var checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+    document.getElementById("alone").checked = false;
+    document.getElementById("everyone").checked = true;
+
     Array.prototype.forEach.call(buttons, function(b) {
         setState(b, states.empty);
+    });
+
+    Array.prototype.forEach.call(checkboxes, function(c) {
+        c.checked = false;
     });
 });
