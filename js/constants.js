d55d0bb3b02ce56d20c353abad623d4a6d40de83
Alana Gilston <abluescarab@users.noreply.github.com>
2021-09-05 17:22:18 -0700

    Updated for v0.3.0

--- js/constants.js
+++ js/constants.js
@@ -9,23 +9,26 @@ const evidence = Object.freeze({
     spiritBox: "spirit-box",
     freezingTemperatures: "freezing-temperatures",
     fingerprints: "fingerprints",
-    ghostOrbs: "ghost-orbs",
-    ghostWriting: "ghost-writing"
+    ghostOrb: "ghost-orb",
+    ghostWriting: "ghost-writing",
+    dotsProjector: "dots-projector"
 });
 
 const ghosts = Object.freeze({
-    spirit: [evidence.ghostWriting, evidence.fingerprints, evidence.spiritBox],
-    wraith: [evidence.fingerprints, evidence.spiritBox, evidence.freezingTemperatures],
-    phantom: [evidence.emfLevel5, evidence.freezingTemperatures, evidence.ghostOrbs],
-    poltergeist: [evidence.fingerprints, evidence.spiritBox, evidence.ghostOrbs],
-    banshee: [evidence.emfLevel5, evidence.fingerprints, evidence.freezingTemperatures],
-    jinn: [evidence.emfLevel5, evidence.spiritBox, evidence.ghostOrbs],
-    mare: [evidence.spiritBox, evidence.freezingTemperatures, evidence.ghostOrbs],
-    revenant: [evidence.emfLevel5, evidence.ghostWriting, evidence.fingerprints],
-    shade: [evidence.emfLevel5, evidence.ghostWriting, evidence.ghostOrbs],
-    demon: [evidence.ghostWriting, evidence.spiritBox, evidence.freezingTemperatures],
-    yurei: [evidence.ghostWriting, evidence.freezingTemperatures, evidence.ghostOrbs],
-    oni: [evidence.emfLevel5, evidence.ghostWriting, evidence.spiritBox],
-    hantu: [evidence.fingerprints, evidence.ghostOrbs, evidence.ghostWriting],
-    yokai: [evidence.spiritBox, evidence.ghostOrbs, evidence.ghostWriting]
+    spirit: [evidence.emfLevel5, evidence.spiritBox, evidence.ghostWriting],
+    wraith: [evidence.emfLevel5, evidence.spiritBox, evidence.dotsProjector],
+    phantom: [evidence.spiritBox, evidence.fingerprints, evidence.dotsProjector],
+    poltergeist: [evidence.spiritBox, evidence.fingerprints, evidence.ghostWriting],
+    banshee: [evidence.fingerprints, evidence.ghostOrb, evidence.dotsProjector],
+    jinn: [evidence.emfLevel5, evidence.fingerprints, evidence.freezingTemperatures],
+    mare: [evidence.spiritBox, evidence.ghostOrb, evidence.ghostWriting],
+    revenant: [evidence.ghostOrb, evidence.ghostWriting, evidence.freezingTemperatures],
+    shade: [evidence.emfLevel5, evidence.ghostWriting, evidence.freezingTemperatures],
+    demon: [evidence.fingerprints, evidence.ghostWriting, evidence.freezingTemperatures],
+    yurei: [evidence.ghostOrb, evidence.freezingTemperatures, evidence.dotsProjector],
+    oni: [evidence.emfLevel5, evidence.freezingTemperatures, evidence.dotsProjector],
+    hantu: [evidence.fingerprints, evidence.ghostOrb, evidence.freezingTemperatures],
+    yokai: [evidence.spiritBox, evidence.ghostOrb, evidence.dotsProjector],
+    goryo: [evidence.emfLevel5, evidence.fingerprints, evidence.dotsProjector],
+    myling: [evidence.emfLevel5, evidence.fingerprints, evidence.ghostWriting]
 });
